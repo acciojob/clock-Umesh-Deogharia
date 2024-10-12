90b28afb38f9797b975c120aea3c3334fd52d45b
Umesh Deogharia <143274287+Umesh-Deogharia@users.noreply.github.com>
2024-10-12 21:46:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let timer = document.getElementById("timer");
+let currentTime = new Date();
+
+setInterval(function() {
+	currentTime = new Date();
+	timer.innerHTML = currentTime.toLocaleString();
+},1000)
+
+
+
+
+
